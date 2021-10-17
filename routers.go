9ad916f9aaef2fd32d5b22aeb9e9f897e0db12c8
theing <934320370@qq.com>
2021-10-17 17:24:47 +0800

    实现登录功能

--- routers.go
+++ routers.go
@@ -11,5 +11,6 @@ import (
 // 接收一个gin 引擎，返回一个引擎
 func CollectRoute(r *gin.Engine) *gin.Engine {
 	r.POST("/api/auth/register", controller.Register) // 导入包中的模块
+	r.POST("/api/auth/login", controller.Login)       // 导入包中的模块
 	return r
 }
