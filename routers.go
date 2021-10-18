e46aab29b402b7deadcc4e49cbf4177625f04caf
theing <934320370@qq.com>
2021-10-18 11:52:19 +0800

    增加中间件路由和获取用户信息的路由

--- routers.go
+++ routers.go
@@ -4,13 +4,16 @@ package main
 
 import (
 	"theing/gin_study/controller"
+	"theing/gin_study/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 // 接收一个gin 引擎，返回一个引擎
 func CollectRoute(r *gin.Engine) *gin.Engine {
-	r.POST("/api/auth/register", controller.Register) // 导入包中的模块
-	r.POST("/api/auth/login", controller.Login)       // 导入包中的模块
+	r.POST("/api/auth/register", controller.Register)                     // 导入包中的模块
+	r.POST("/api/auth/login", controller.Login)                           // 用户登录
+	r.GET("/api/auth/info", middleware.AuthMiddleware(), controller.Info) // 利用中间件包含用户的信息。
+
 	return r
 }
